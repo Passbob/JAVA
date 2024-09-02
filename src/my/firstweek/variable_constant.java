6dfca1a35ba347c9f0dd6a191bb9c459e0c715cd
i7E-88 <gudjtr097@gmail.com>
2024-09-02 17:43:07 +0900

    9/2

--- src/my/firstweek/variable_constant.java
+++ src/my/firstweek/variable_constant.java
@@ -6,6 +6,13 @@ public class variable_constant {
     * 변수는 값이 변할 수 있고 간단하고 편리하게 표현하기 위해서 필요하다
     * */
 
+        /*sout = System.out.printIn();을 불러오는 단축키*/
+
+        // soutv = 위의 값을 sout에 바로 불러올 수 있는 단축키`
+
+        //위처럼 스택이 쌓이는데 그걸 초기화 하고 다시 하는 방법은? -> 한 달 주기마다 사람들이 구매한 값을 초기화 시키기`
+
+
         //자료형
 
         //정수
@@ -38,15 +45,39 @@ public class variable_constant {
         //언더플로우는 반대로 최대값으로 순환시킨 뒤 거기서부터 계산`
 
 
+        //데이터 형변환
+        //자동 형변환과 강제 형변환이 있다. 자바에서 같은 데이터 타입끼리만 연산을 수행할 수 있기 때문에 변환을 해야한다.
 
-        /*sout = System.out.printIn();을 불러오는 단축키*/
+        //자동 형변환
+        //작은 자료형에서 큰 자료형으로는 자동으로 형변환 된다. -> 같은 바이트도 안됨
+        //정수에서 실수로 가능
+        //문자는 int형으로 자동 형변환
+        //논리형은 형변환 규칙에서 제외된다.
 
-        // soutv = 위의 값을 sout에 바로 불러올 수 있는 단축키`
+        //강제 형변환
+        //위를 제외한 상황에서는 강제형변환이 필요하지만 논리형은 제외다.
 
-        //위처럼 스택이 쌓이는데 그걸 초기화 하고 다시 하는 방법은? -> 한 달 주기마다 사람들이 구매한 값을 초기화 시키기`
+        //형변환 시 데이터 손실에 유의해야하는데 의도해서 데이터를 손실시킬 수 있다. ex)소수점 없애기
 
+//        a사원의 salary + bonus 만들자
 
+        String employee = "조형석";
+        int salary = 1000000;
+        int bonus = 500000;
 
+        System.out.println(employee+"사원의 이번 달 월급은 "+(salary + bonus)+"원 입니다.");
+
+        long total = salary + bonus;
+
+        System.out.println(employee+"가 최우수 사원이 되어 보너스를 더블로 받았네요! 이번달 월급은 "+(total + bonus)+"입니다!");
+
+        long triple = total + bonus;
+
+        System.out.println("사장님이 미쳤어요! "+ employee+"가 트리플로 받았네요! 이번달 월급은 "+(triple + bonus)+"입니다!");
+
+        int min = (int)triple - bonus*2;
+
+        System.out.println("아...사장님이 화났어요..." + min + "가 최종 월급입니다...");
 
     }
 }
