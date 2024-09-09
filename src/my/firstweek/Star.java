ac499a797160a6c484f757eda983ac51b02358ee
Passbob <gudjtr097@gmail.com>
2024-09-09 09:19:15 +0900

    Add files via upload

---
+++ src/my/firstweek/Star.java
@@ -0,0 +1,52 @@
+package my.firstweek;
+
+import java.util.Scanner;
+
+public class Star {
+
+    public void treeMaker(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("원하는 트리 높이를 숫자로 입력하세요 : ");
+
+        int height = sc.nextInt();
+
+
+        for(int i = 1; i <= height; i++){
+            for(int j = height-i; j >=0; j--){
+                System.out.print(" ");
+            }
+            for(int star = 1; star <= 1+(i-1)*2; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("⸜(*ˊᗜˋ*)⸝ 트리가 완성되었습니다!! ");
+
+    }
+        public void treeMaker1(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("원하는 트리 높이를 숫자로 입력하세요 : ");
+
+        int height = 10;
+
+
+        for(int i = 1; i <= height; i++){
+            for(int j = height-i; j >=0; j--){
+                System.out.print(" ");
+            }
+            for(int star = 1; star <= 1+(i-1)*2; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("⸜(*ˊᗜˋ*)⸝ 트리가 완성되었습니다!! ");
+
+    }
+
+
+
+}
