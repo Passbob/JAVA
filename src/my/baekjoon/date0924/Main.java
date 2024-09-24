7b1e71ded31305eff99eb57473d08ebcf1bd4ba8
DESKTOP-KLQ0O04\admin <gudjtr097@gmail.com>
2024-09-24 23:42:00 +0900

    9/22

---
+++ src/my/baekjoon/date0924/Main.java
@@ -0,0 +1,34 @@
+package my.baekjoon.date0924;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    y = arr[j];
+                    x = arr[i];
+                }
+            }
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == x || arr[j] == y) {
+                continue;
+            }
+            System.out.println(arr[j]);
+        }
+    }
+}
